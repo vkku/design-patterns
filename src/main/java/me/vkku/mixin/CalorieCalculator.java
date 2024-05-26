23a43a104242adfe90831b69e2592b17381454b8
Vivek Kumar <contactviku@gmail.com>
2024-05-26 20:42:32 +0530

    Add crisp definition

--- src/main/java/me/vkku/mixin/CalorieCalculator.java
+++ src/main/java/me/vkku/mixin/CalorieCalculator.java
@@ -3,6 +3,8 @@ package me.vkku.mixin;
 import java.util.List;
 import me.vkku.mixin.IngredientList.Ingredient;
 /*
+    Core : Add additional functionality to existing type(interface). Can also be described as optional interface to implement
+    from the POV of concrete class, as the class(concrete) would have an option to implement existing or mixin type.
     Mixin is a optional interface which allows one to use alternatives in implementation, mixins are abstract and often
     implemented using multiple inheritance. Java does not support multiple inheritance hence I used the following approach
     to use a library collection List with an additional method getElements. so ideally getElements is mixed to default implementation,
